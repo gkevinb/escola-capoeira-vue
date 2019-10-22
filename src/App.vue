4577d8959d1fb5bced50ae24e9af1641afe28b32
Gabor Kevin Barta <gkevinba@gmail.com>
2019-10-22 02:37:35 +0200

    Update frontend and minor refactor

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Escola Capoeira"/>
+    <Intro msg="ESCOLA CAPOEIRA"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Intro from './components/Intro.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Intro
   }
 }
 </script>
