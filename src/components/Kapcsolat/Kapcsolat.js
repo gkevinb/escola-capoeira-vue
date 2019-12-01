ab2166029a96912e4ff7b45dee6f2022732cf2e3
Gabor Kevin Barta <gkevinba@gmail.com>
2019-12-01 15:27:47 +0100

    Add contact page content

--- src/components/Kapcsolat/Kapcsolat.js
+++ src/components/Kapcsolat/Kapcsolat.js
@@ -2,12 +2,41 @@ export default {
     name: "kapcsolat-tab",
     data() {
         return {
+            contacts: [
+                {
+                    name: 'Tocha Fabiano',
+                    capoeiraName: 'Contramestre Tocha',
+                    facebookLink: 'https://www.facebook.com/tocha.cecab',
+                    facebookId: '100000954874709'
+                },
+                {
+                    name: 'Ács Bea',
+                    capoeiraName: 'Graduada Bea',
+                    facebookLink: 'https://www.facebook.com/bea.acs.5',
+                    facebookId: '615787357',
+                    phone: '+36302517231'
+                },
+                {
+                    name: 'Botos András',
+                    capoeiraName: 'Estágiário Furacão',
+                    facebookLink: 'https://www.facebook.com/andras.botos.3',
+                    facebookId: '100001537112922'
+                },
+            ]
         };
     },
     created() {
 
     },
     methods: {
-
+        formatPhoneLink: function(phoneNumber) {
+            return 'tel:' + phoneNumber
+        },
+        redirectFacebook: function (contact) {
+            setTimeout(function () {
+                window.location.assign(contact.facebookLink)
+            }, 200);
+            window.location.assign('fb://profile/' + contact.facebookId)
+        }
     }
 };
